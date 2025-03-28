09d8b2954a32145587fa15b32c2371fed2ca3b48
MBEA <matzabateextraterestriiantici@protonmail.com>
2025-03-28 00:58:29 -0700

    Change to use the testing vars from github variables

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,11 +13,14 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? testVars.retry_count : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? testVars.workers_count : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-reports/html-report', open: 'never' }], // HTML report
+    ['json', { outputFile: 'playwright-reports/json-report/results.json' }] // JSON report
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: testVars.timeoutExagerate, //  timeout
   use: {
